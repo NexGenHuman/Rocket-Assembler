0b7e7247b8000c2173da695e59741d149d70c781
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-10-22 15:52:06 +0200

    Created rocket class

---
+++ RocketAssembler/UtilityClasses/Rocket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketAssembler.UtilityClasses
+{
+    class Rocket
+    {
+        Capsule capsule;
+        Orbital_Stage orbital_stage;
+        Main_Stage main_stage;
+        Solid_Fuel_Booster solid_fuel_booster;
+
+        public Rocket(Capsule c, Orbital_Stage os, Main_Stage ms, Solid_Fuel_Booster sfb)
+        {
+            capsule = c;
+            orbital_stage = os;
+            main_stage = ms;
+            solid_fuel_booster = sfb;
+        }
+    }
+}
